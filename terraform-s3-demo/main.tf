6ec1d4e2736952467e57a4f15d8d32cd9a9bb259
gavinbarbee <gavinbarbee@gmail.com>
2026-04-23 09:51:52 -0700

    feat: add clean Terraform project (S3 static website IaC) chore: ignore .terraform and .tfstate files

---
+++ terraform-s3-demo/main.tf
@@ -0,0 +1,58 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
+  upper   = false
+}
+
+resource "aws_s3_bucket" "static_site" {
+  bucket = "gavin-terraform-demo-${random_string.suffix.result}"
+}
+
+resource "aws_s3_bucket_website_configuration" "static_site" {
+  bucket = aws_s3_bucket.static_site.id
+
+  index_document {
+    suffix = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "static_site" {
+  bucket = aws_s3_bucket.static_site.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "static_site" {
+  bucket = aws_s3_bucket.static_site.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.static_site.arn}/*"
+      }
+    ]
+  })
+}
+
+resource "aws_s3_object" "index" {
+  bucket       = aws_s3_bucket.static_site.id
+  key          = "index.html"
+  source       = "index.html"
+  content_type = "text/html"
+}
+
+output "website_url" {
+  value = "http://${aws_s3_bucket_website_configuration.static_site.website_endpoint}"
+}
